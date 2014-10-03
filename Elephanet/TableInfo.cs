94d2f231415730f40873a0ef7c8600a14b03bcb0
James Kelly <james.kelly.99@gmail.com>
2014-10-03 17:37:30 +1000

    small class to generate table names from types

---
+++ Elephanet/TableInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Elephanet
+{
+    public class TableInfo
+    {
+        private string _schema;
+
+        public TableInfo()
+        {
+            _schema = "public";
+        }
+
+        public TableInfo(string schema)
+        {
+            _schema = schema;
+        }
+
+        public string TableNameWithSchema(Type type)
+        {
+            return String.Format("{0}.{1}_{2}", _schema, type.Namespace.ReplaceDotWithUnderscore().ToLower(), type.Name.ToLower());
+        }
+
+        public string TableNameWithoutSchema(Type type)
+        {
+            return String.Format("{0}_{1}", type.Namespace.ReplaceDotWithUnderscore().ToLower(), type.Name.ToLower());
+        }
+
+        public string Schema
+        {
+            get
+            {
+                return _schema;
+            }
+        }
+    }
+}
