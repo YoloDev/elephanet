f7b301338cf3942ae25f499125fe43f028a04a0d
James Kelly <james.kelly.99@gmail.com>
2014-10-03 17:55:26 +1000

    add tests for store info

---
+++ Elephanet.Tests/StoreInfoTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Elephanet;
+using Xunit;
+using Shouldly;
+
+namespace Elephanet.Tests
+{
+    public class StoreInfoTests
+    {
+        [Fact]
+        public void StoreInfo_GivenANewTable_Should_AddItToItsTableNamesList()
+        {
+            StoreInfo storeInfo = new StoreInfo();
+            storeInfo.Add("someting or rather");
+
+            storeInfo.Tables.ShouldContain("someting or rather");
+        }
+    }
+}
