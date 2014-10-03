723e19b3436e569304a10ab8d995822a345add05
James Kelly <james.kelly.99@gmail.com>
2014-10-03 17:55:54 +1000

    add tests for table info

---
+++ Elephanet.Tests/TableInfoTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Shouldly;
+
+namespace Elephanet.Tests
+{
+    public class ConventionTests
+    {
+        [Fact]
+        public void TableInfo_GivenNoSchema_ShouldReturnSchemaOfPublic()
+        {
+            TableInfo tableInfo = new TableInfo();
+            tableInfo.Schema.ShouldBe("public");
+        }
+
+        [Fact]
+        public void TableInfo_GivenASchema_ShouldReturnThatSchema()
+        {
+            TableInfo tableInfo = new TableInfo("aschema");
+            tableInfo.Schema.ShouldBe("aschema");
+        }
+
+        [Fact]
+        public void TableInfo_GivenAType_ShouldReturn_TheCorrectTableNameWithoutSchema()
+        {
+            TableInfo tableInfo = new TableInfo("aschema");
+            tableInfo.TableNameWithoutSchema(typeof(Car)).ShouldBe("elephanet_tests_car");
+ 
+        }
+
+        [Fact]
+        public void TableInfo_GivenAType_ShouldReturn_TheCorrectTableNameWithSchema()
+        {
+            TableInfo tableInfo = new TableInfo("aschema");
+            tableInfo.TableNameWithSchema(typeof(Car)).ShouldBe("aschema.elephanet_tests_car");
+
+        }
+    }
+}
