51094cab7e108dd66125b06897f3e47fca8beb64
James Kelly <james.kelly.99@gmail.com>
2015-03-01 22:14:59 +1100

    move serializers to their own directory

---
+++ Elephanet/Serialization/IJsonConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Elephanet.Serialization
+{
+    public interface IJsonConverter
+    {
+        string Serialize<T>(T entity);
+        T Deserialize<T>(string json);
+        object Deserialize(string json);
+        object Deserialize(string json, Type type);
+    }
+}
