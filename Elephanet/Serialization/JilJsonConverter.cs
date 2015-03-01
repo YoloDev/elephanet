d7a3ec908e4d97d56bec2246a13bce3d1e25f154
James Kelly <james.kelly.99@gmail.com>
2015-03-01 22:15:34 +1100

    add a Jil Json Serializer / Deserializer

---
+++ Elephanet/Serialization/JilJsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using Jil;
+
+namespace Elephanet.Serialization
+{
+    public class JilJsonConverter : IJsonConverter
+    {
+        public string Serialize<T>(T entity)
+        {
+            return JSON.Serialize<T>(entity);
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            return JSON.Deserialize<T>(json);
+        }
+
+        public object Deserialize(string json)
+        {
+            return JSON.DeserializeDynamic(json);
+        }
+
+        public object Deserialize(string json, System.Type type)
+        {
+            return JSON.Deserialize(json, type);
+        }
+    }
+}
