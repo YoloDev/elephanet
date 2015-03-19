6973cdeaf34249dc3471fbad08199f0c47cf7d14
James Kelly <james.kelly.99@gmail.com>
2015-03-19 20:44:59 +1100

    add a test for GetAll<T> and make sure it doesnt throw when no table is present (auto creates it)

---
+++ Elephanet.Tests/GetAllTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace Elephanet.Tests
+{
+    public class GetAllTests
+    {
+
+        [Fact]
+        public void GetAllCreatesTheTableIfItDoesNotExist()
+        {
+            var store = new TestStore();
+            using (var session = store.OpenSession())
+            {
+                var things = session.GetAll<SomeTestClassThatNoOneWillEverUse>().ToList();
+                things.Count.ShouldBe(0);
+            }
+        }
+    }
+
+
+    public class SomeTestClassThatNoOneWillEverUse
+    {
+        public Guid Id { get; set; }
+        public string Something { get; set; }
+    }
+}
