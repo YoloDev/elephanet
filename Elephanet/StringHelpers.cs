d4140cc0c7778d418c1f464b0e8add7489b21267
James Kelly <james.kelly.99@gmail.com>
2015-04-16 21:45:30 +1000

    add extension to quote a quote in a string (needed for pg to properly handle quotes)

---
+++ Elephanet/StringHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace Elephanet.Helpers
+{
+    public static class StringHelpers
+    {
+        public static string EscapeQuotes(this string text)
+        {
+            return text.Replace("'", "''");
+        }
+    }
+}
