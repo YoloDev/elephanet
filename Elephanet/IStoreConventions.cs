6882da0f0a39396befd39a1618f64185161c3f32
Frank Wise <frankwise@gmail.com>
2015-09-08 08:12:24 -0700

    Added support on IStoreConventions/StoreConventions for a value that controls the behavior of the DocumentSession.GetById when entity is not found. The behavior of GetById is not configurable (yet) in this commit.

--- Elephanet/IStoreConventions.cs
+++ Elephanet/IStoreConventions.cs
@@ -1,9 +1,11 @@
-﻿using Elephanet.Serialization;
+﻿using Elephanet.Conventions;
+using Elephanet.Serialization;
 namespace Elephanet
 {
     public interface IStoreConventions
     {
         IJsonConverter JsonConverter { get; }
         ITableInfo TableInfo { get; }
+        EntityNotFoundBehavior EntityNotFoundBehavior { get; }
     }
 }
